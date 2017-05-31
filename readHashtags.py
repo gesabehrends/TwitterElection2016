4e532dbafc12bd0e3b6dd27f2c3303d61aab4850
Gesa Behrends <gesa.behrends@googlemail.com>
2017-05-31 13:45:11 +0200

    prepare csv files

---
+++ readHashtags.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import re
+#Files oeffnen (input zum lesen, output zum schreiben)
+inputFile = open("american-election-tweets_02.csv", "r")
+outputFileAll = open("tweetsPreliminary.csv", "w")
+outputFileHashtags = open("hashtags.csv", "w")
+
+#Maximale Anzahl Hashtags pro Tweet zaehlen, um passenden Header
+#anzulegen
+numberHashtags = 0
+for line in inputFile:
+    if(line.count('#') > numberHashtags):
+        numberHashtags = line.count('#')
+
+addToHeader = ""      
+for i in range(0, numberHashtags):
+    addToHeader += ";hashtag_"+str(i)
+
+#Read-File auf die erste Zeile setzen, alle Hashtags auslesen und in neuen
+#Spalten anlegen
+inputFile.seek(0)        
+        
+headerTweets = inputFile.readline()
+headerTweets = headerTweets.rstrip("\n") + addToHeader
+headerTweets = headerTweets +"\n"
+outputFileAll.write(headerTweets)
+
+allHashtags = []
+for line in inputFile:
+    listHashtags = re.findall(r"#(\w+)", line)
+    allHashtags += listHashtags
+    strHashtags = ""
+    for hashtag in listHashtags:
+        strHashtags += hashtag + ";"
+    line = line.rstrip("\n") + ";" + strHashtags.rstrip(" ").rstrip(";") + "\n"
+    outputFileAll.write(line)
+
+#Herausfinden, welche unterschiedlichen Hashtags eingesetzt wurden
+allHashtags = list(set(allHashtags))    
+
+#Tabelle fuer Hashtags anlegen
+headerHashtags = "hashtag_ID;hashtag_content\n"
+outputFileHashtags.write(headerHashtags)
+i = 0
+    
+for hashtag in allHashtags:
+     i+=1
+     line = str(i) + ";" + hashtag +"\n"
+     outputFileHashtags.write(line)
+    
+    
+print("Hashtags written to file, max " +str(numberHashtags)+ " hashtags per Tweet, "
++ str(i) +" unique Hashtags used")
---
+++ workOnData.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import pandas
+from datetime import date, datetime
+
+
+#Daten einlesen
+allDataTweets = pandas.read_csv("tweetsPreliminary.csv", sep=';', header = 0, encoding = "latin-1")
+hashtags = pandas.read_csv("hashtags.csv", sep=';', header = 0, encoding = "latin-1")
+
+#Fuer die Tweets-Auspraegung alle nicht benoetigten Attribute entfernen, Timestamp berechnen
+allDataTweets = allDataTweets.drop(["text", "is_retweet", "original_author", "in_reply_to_screen_name",
+ "is_quote_status", "source_url", "truncated"], axis=1)
+allDataTweets = allDataTweets.rename(columns={"handle": "autor"})
+
+allDataTweets.insert(0, "Tweet_ID", range(1, len(allDataTweets)+1))  
+tweets = allDataTweets
+tweets = tweets.drop(["hashtag_0", "hashtag_1", "hashtag_2", "hashtag_3", "hashtag_4", 
+"hashtag_5"], axis=1)
+
+##TO DO: Timestamp berechnen aus dem Datum - Wie die Timezone beruecksichtigen?
+
+#s = "2016-09-28T00:22:34"
+#time.mktime(datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%S").timetuple())
+
+#Enthaelt-Aupraegung aufstellen
+
+enthaelt = allDataTweets
+enthaelt = enthaelt.drop(["autor",	"time", "retweet_count", "favorite_count"], axis =1)
+
+#Alle Hashtags in einer (langen Spalte verschmelzen)
+enthaelt = pandas.melt(enthaelt, id_vars=["Tweet_ID"])
+enthaelt = enthaelt.rename(columns={"value": "hashtag_content"})
+
+#die enthaelt-Tabelle mit der Hashtag-Tabelle verschmelzen, um die Schluessel zusammen-
+#zubringen. (Inner join, da wir nur Tweet-IDs behalten wollen, in denen tatsaechlich
+#Hashtags auftreten)
+
+enthaelt = pandas.merge(enthaelt, hashtags, how="inner", on="hashtag_content")
+enthaelt = enthaelt.drop(["variable","hashtag_content"], axis=1)
+
+#Tabellen schreiben
+enthaelt.to_csv("enthaelt.csv", sep=';', index=False)
+tweets.to_csv("tweets.csv", sep=';', index=False)
